f8361ea2bc7f6f98469013a2b77e62bca9fd3aa1
Hugo Baraúna <hugo.barauna@plataformatec.com.br>
2013-12-17 16:34:45 -0200

    section 11.3

--- features/step_definitions/game_steps.rb
+++ features/step_definitions/game_steps.rb
@@ -8,6 +8,11 @@ Quando /^começo um novo jogo$/ do
   start_new_game
 end
 
+Quando /^escolho que a palavra a ser sorteada deverá ter "(.*?)" letras\
+$/ do |number_of_letters|
+  type(number_of_letters)
+end
+
 Então /^vejo na tela:$/ do |text|
   assert_partial_output(text, all_stdout)
 end
