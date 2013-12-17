ae63f571e2b5eba685c4f6aaa72922ebcb5bbe38
Hugo Baraúna <hugo.barauna@plataformatec.com.br>
2013-12-17 16:52:52 -0200

    section 11.5

--- lib/game.rb
+++ lib/game.rb
@@ -3,10 +3,19 @@
 class Game
   def initialize(output = STDOUT)
     @output = output
+    @ended = false
   end
 
   def start
     initial_message = "Bem-vindo ao jogo da forca!"
     @output.puts initial_message
+  end
+
+  def ended?
+    @ended
+  end
+
+  def next_step
+    @output.puts("Qual o tamanho da palavra a ser sorteada?")
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,16 +4,33 @@ require 'spec_helper'
 require 'game'
 
 describe Game do
+  let(:output) { double("output") }
+
+  subject(:game) { Game.new(output) }
+
   describe "#start" do
     it "prints the initial message" do
-      output = double("output")
-      game = Game.new(output)
-
       initial_message = "Bem-vindo ao jogo da forca!"
       expect(output).to receive(:puts).with(initial_message)
 
-
       game.start
+    end
+  end
+
+  describe "#ended?" do
+    it "returns false when the game just started" do
+      expect(game).not_to be_ended
+    end
+  end
+
+  describe "#next_step" do
+    context "when the game just started" do
+      it "asks the player for the length of the word to be raffled" do
+        question = "Qual o tamanho da palavra a ser sorteada?"
+        expect(output).to receive(:puts).with(question)
+
+        game.next_step
+      end
     end
   end
 end
