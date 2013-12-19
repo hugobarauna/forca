26943d70c8196cfdedd609f2ce3e7d313abb87f2
Hugo Baraúna <hugo.barauna@plataformatec.com.br>
2013-12-19 11:27:32 -0200

    section 14.3

--- features/step_definitions/game_steps.rb
+++ features/step_definitions/game_steps.rb
@@ -1,7 +1,18 @@
 # encoding: UTF-8
 
+Dado /^o jogo tem as possíveis palavras para sortear:$/ do |words_table|
+  words = words_table.rows.map(&:last).join(" ")
+  set_rafflable_words(words)
+end
+
 Dado /^que comecei um jogo$/ do
   start_new_game
+end
+
+Dado /^que escolhi que a palavra a ser sorteada deverá ter "(.*?)"\
+ letras\
+$/ do |number_of_letters|
+  type(number_of_letters)
 end
 
 Quando /^começo um novo jogo$/ do
@@ -13,6 +24,9 @@ $/ do |number_of_letters|
   type(number_of_letters)
 end
 
+Quando /^tento adivinhar que a palavra tem a letra "(.*?)"$/ do |letter|
+  type(letter)
+end
 
 Quando /^termino o jogo$/ do
   type("fim")
@@ -20,4 +34,9 @@ end
 
 Então /^o jogo termina com a seguinte mensagem na tela:$/ do |text|
   assert_passing_with(text)
+end
+
+Então /^o jogo mostra que eu adivinhei uma letra com sucesso$/ do
+  assert_partial_output("Você adivinhou uma letra com sucesso.",
+                         all_stdout)
 end
--- features/support/game_helpers.rb
+++ features/support/game_helpers.rb
@@ -1,6 +1,12 @@
 module GameHelpers
+  def set_rafflable_words(words)
+    @raffable_words = words
+  end
+
   def start_new_game
-    run_interactive("forca")
+    set_rafflable_words(%w[hi mom game fruit]) if @raffable_words.nil?
+
+    run_interactive("forca \"#{@raffable_words}\"")
   end
 end
 
