aadd0061ed9cf18c26dfdbc1b6ae23300c0c603b
Philippe Hardardt <phil.henri@gmail.com>
2019-03-11 21:12:36 -0300

    Replace last_command_started with last_command_stopped.
    Some times last_command_started doesn't wait for the program to finish,
    causing the tests to fail.

--- features/step_definitions/game_steps.rb
+++ features/step_definitions/game_steps.rb
@@ -37,16 +37,16 @@ Quando "termino o jogo" do
 end
 
 Então "o jogo termina com a seguinte mensagem na tela:" do |text|
-  expect(last_command_started).to be_successfully_executed
-  expect(last_command_started.output).to include(text)
+  expect(last_command_stopped).to be_successfully_executed
+  expect(last_command_stopped.output).to include(text)
 end
 
 Então "o jogo mostra que eu adivinhei uma letra com sucesso" do
-  expect(last_command_started).to have_output(
+  expect(last_command_stopped).to have_output(
     /Você adivinhou uma letra com sucesso./
   )
 end
 
 Então "o jogo mostra que eu errei a adivinhação da letra" do
-  expect(last_command_started).to have_output(/Você errou a letra./)
+  expect(last_command_stopped).to have_output(/Você errou a letra./)
 end
