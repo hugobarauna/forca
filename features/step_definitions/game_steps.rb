b03b7c768e3e5b0eee3e42eccecabe11f39322b8
Philippe Hardardt <phil.henri@gmail.com>
2019-03-21 22:38:00 -0300

    Check stdout instead of output.

--- features/step_definitions/game_steps.rb
+++ features/step_definitions/game_steps.rb
@@ -38,15 +38,15 @@ end
 
 Então "o jogo termina com a seguinte mensagem na tela:" do |text|
   expect(last_command_stopped).to be_successfully_executed
-  expect(last_command_stopped.output).to include(text)
+  expect(last_command_stopped.stdout).to include(text)
 end
 
 Então "o jogo mostra que eu adivinhei uma letra com sucesso" do
-  expect(last_command_stopped).to have_output(
-    /Você adivinhou uma letra com sucesso./
+  expect(last_command_stopped.stdout).to include(
+    "Você adivinhou uma letra com sucesso."
   )
 end
 
 Então "o jogo mostra que eu errei a adivinhação da letra" do
-  expect(last_command_stopped).to have_output(/Você errou a letra./)
+  expect(last_command_stopped.stdout).to include("Você errou a letra.")
 end
